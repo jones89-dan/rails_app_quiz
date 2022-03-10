b2fd6546513d640e4a0514eb45850cb2e8e38e67
jones89-dan <jones.89.dan@gmail.com>
2022-03-10 15:47:07 -0800

    Created Routes for API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/',  to: 'static_pages#index'
+  post '/jobs', to: ':jobs#create'
+  get '/jobs/:id', to: 'jobs#show'
+  get '/jobs', to: 'jobs#index'
+
 end
